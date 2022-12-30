3d230c51222b680f238d5c667f9190c323fe4846
jjeda <jjeda@naver.com>
2022-12-31 00:08:53 +0900

    [Programmer] 제네릭 적용
    - ServerClientPaper 부분을 해결 실패

--- src/oop/programmer/Paper.kt
+++ src/oop/programmer/Paper.kt
@@ -1,26 +1,24 @@
 package oop.programmer
 
-interface Paper {
-  fun setData(programmer: Programmer)
+interface Paper<T : Programmer> {
+  fun setData(programmer: T)
 }
 
 class ClientPaper(
   var programmer: Programmer
-) : Paper {
+) : Paper<FrontEndProgrammer> {
   private val library = Library("vueJS")
   private val language = Language("kotlinJS")
-  override fun setData(programmer: Programmer) {
-    if (programmer is FrontEndProgrammer) {
-      programmer.setLibrary(library)
-      programmer.setLanguage(language)
-    }
+  override fun setData(programmer: FrontEndProgrammer) {
+    programmer.setLibrary(library)
+    programmer.setLanguage(language)
   }
 }
 
 class ServerClientPaper(
   var frontEndProgrammer: Programmer,
   var backEndProgrammer: Programmer,
-) : Paper {
+) : Paper<FrontEndProgrammer, BackEndProgrammer> { // 이 부분을 해결할 수 없음..
   private val server = Server("test")
   private val frontEndLanguage = Language("kotlin")
   private val backEndLanguage = Language("kotlin")
