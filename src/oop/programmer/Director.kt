598a15c09afbb61b3bb43600e59c46647d92577d
jjeda <jjeda@naver.com>
2022-12-31 01:36:59 +0900

    [Programmer] client 방향으로 구현부 이동

--- src/oop/programmer/Director.kt
+++ src/oop/programmer/Director.kt
@@ -6,38 +6,9 @@ class Director {
   fun addProject(name: String, paper: Paper) { projects[name] = paper }
 
   fun runProject(name: String) {
-    val paper = projects[name]
-
-    if (paper is ServerClientPaper) { // 이제 이 부분이 OCP 위반
-      val project: ServerClientPaper = paper
-      val frontEndProgrammer = object : FrontEndProgrammer<ServerClientPaper>() {
-        override fun setData(paper: ServerClientPaper) {
-            language = paper.frontEndLanguage
-        }
-      }
-
-      val backEndProgrammer = object : BackEndProgrammer<ServerClientPaper>() {
-        override fun setData(paper: ServerClientPaper) {
-          server = paper.server
-          language = paper.backEndLanguage
-        }
-      }
-      project.setFrontEndProgrammer(frontEndProgrammer)
-      project.setBackEndProgrammer(backEndProgrammer)
-      val client = frontEndProgrammer.getProgram(project)
-      val server = backEndProgrammer.getProgram(project)
-      deploy(name, client, server)
-    } else if (paper is ClientPaper) {
-      val project: ClientPaper = paper
-      val frontEndProgrammer = object : FrontEndProgrammer<ClientPaper>() {
-        override fun setData(paper: ClientPaper) {
-          library = paper.library
-          language = paper.language
-        }
-      }
-      project.setProgrammer(frontEndProgrammer)
-      deploy(name, frontEndProgrammer.getProgram(project))
-    }
+    val paper = projects[name] ?: throw RuntimeException()
+    deploy(name, *paper.run().toTypedArray())
   }
-  private fun deploy(projectName: String, vararg programs: Program) {}
 }
+
+private fun deploy(projectName: String, vararg programs: Program) {}
--- src/oop/programmer/Paper.kt
+++ src/oop/programmer/Paper.kt
@@ -1,30 +1,21 @@
 package oop.programmer
 
-interface Paper
-
-class ClientPaper() : Paper {
-  val library = Library("vueJS")
-  val language = Language("kotlinJS")
-  private lateinit var programmer: Programmer<ClientPaper>
-  fun setProgrammer(programmer: Programmer<ClientPaper>) {
-    this.programmer = programmer
-  }
+interface Paper {
+  fun run(): List<Program>
 }
 
-class ServerClientPaper() : Paper {
+abstract class ClientPaper() : Paper {
+  val library = Library("vueJS")
+  val language = Language("kotlinJS")
+  protected lateinit var programmer: Programmer<ClientPaper>
+}
+
+abstract class ServerClientPaper() : Paper {
   val server = Server("test")
   val frontEndLanguage = Language("kotlin")
   val backEndLanguage = Language("kotlin")
-  private lateinit var frontEndProgrammer: Programmer<ServerClientPaper>
-  private lateinit var backEndProgrammer: Programmer<ServerClientPaper>
-
-  fun setBackEndProgrammer(programmer: Programmer<ServerClientPaper>) {
-    this.backEndProgrammer = programmer
-  }
-
-  fun setFrontEndProgrammer(programmer: Programmer<ServerClientPaper>) {
-    this.frontEndProgrammer = programmer
-  }
+  protected lateinit var frontEndProgrammer: Programmer<ServerClientPaper>
+  protected lateinit var backEndProgrammer: Programmer<ServerClientPaper>
 }
 
 data class Library(
@@ -38,3 +29,42 @@ data class Language(
 data class Server(
   private val title: String
 )
+
+fun main() {
+  val director = Director()
+  director.addProject("프론트", object : ClientPaper() {
+    override fun run(): List<Program> {
+      val frontEndProgrammer = object : FrontEndProgrammer<ClientPaper>() {
+        override fun setData(paper: ClientPaper) {
+          library = paper.library
+          language = paper.language
+        }
+      }
+      // setProgrammer(frontEndProgrammer)
+      programmer = frontEndProgrammer // 접근레벨 조정
+      return listOf(frontEndProgrammer.getProgram(this))
+    }
+  })
+  director.runProject("프론트")
+
+  director.addProject("프론트 서버", object : ServerClientPaper() {
+    override fun run(): List<Program> {
+      val frontEndProgrammer = object : FrontEndProgrammer<ServerClientPaper>() {
+        override fun setData(paper: ServerClientPaper) {
+          language = paper.frontEndLanguage
+        }
+      }
+
+      val backEndProgrammer = object : BackEndProgrammer<ServerClientPaper>() {
+        override fun setData(paper: ServerClientPaper) {
+          server = paper.server
+          language = paper.backEndLanguage
+        }
+      }
+      this.frontEndProgrammer = frontEndProgrammer
+      this.backEndProgrammer = backEndProgrammer
+      return listOf(frontEndProgrammer.getProgram(this), backEndProgrammer.getProgram(this))
+    }
+  })
+  director.runProject("프론트 서버")
+}
